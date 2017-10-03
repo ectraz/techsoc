36fff3c7ff5babc6fbde99098d8762161cc45366
bensimes <bensimes1@gmail.com>
2017-10-03 16:56:09 +0100

    Change in file type
    Tested in mamp, works fine with the about us page

--- navbar.php
+++ navbar.php
@@ -16,7 +16,7 @@
     </div>
     <ul class="nav justify-content-center">
       <li class="nav-item">
-        <a class="nav-link" href="#">About Us</a>
+        <a class="nav-link" href="about_us">About Us</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#">Events</a>
